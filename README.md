58090d0703278966cbe3f73567ecbec23f8f1903
Fearon8 <alcalgon@gmail.com>
2015-06-20 18:12:44 +0200

    clean up

--- README.md
+++
@@ -1,2 +0,0 @@
-# FactoryCraft
-A Tech Mod
--- src/main/java/com/github/Fearon8/FactoryCraft/tabs/ModTabs.java
+++
@@ -1,9 +0,0 @@
-package com.github.Fearon8.FactoryCraft.tabs;
-
-import net.minecraft.creativetab.CreativeTabs;
-
-public class ModTabs 
-{
-	public static CreativeTabs tabFactoryCraft = new FactoryCraftTab(CreativeTabs.getNextID(), "tabFactoryCraft");
-	
-}
